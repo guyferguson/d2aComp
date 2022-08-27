7d5dcbe030a9c0117a3c9876eb530bf55b79dec4
guyferguson <guyferguson@tpg.com.au>
2022-08-27 18:23:22 +1000

    nuGet package updates for security dependabot alerts

--- d2aComp.Tests/Controllers/HomeControllerTest.cs
+++ d2aComp.Tests/Controllers/HomeControllerTest.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using WebApplication1;
 using WebApplication1.Controllers;
 
 namespace d2aComp.Tests.Controllers
